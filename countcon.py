58f79a2e6ee49683b861c45f330caf263d0d73db
jai windows <60311419+Nattarrud1178@users.noreply.github.com>
2021-06-16 15:43:17 +0700

    sync laptop

--- countcon.py
+++ countcon.py
@@ -1,32 +1,29 @@
 from imutils import contours
 import imutils
 import cv2
+import numpy as np
+from numpy.core.fromnumeric import reshape
 
 
+image = cv2.imread('testtt.jpg')
+hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
-image = cv2.imread('test.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (7, 7), 0)
+lower_white = np.array([0,0,180], dtype=np.uint8)
+upper_white = np.array([180,50,255], dtype=np.uint8)
 
-edged = cv2.Canny(gray, 50, 100)
-edged = cv2.dilate(edged, None, iterations=1)
-edged = cv2.erode(edged, None, iterations=1)
+mask = cv2.inRange(hsv,lower_white,upper_white)
+res = cv2.bitwise_and(image,image,mask=mask)
 
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-    cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
+open = cv2.morphologyEx(
+        res,
+        cv2.MORPH_OPEN,
+        np.ones((7,7),
+        np.uint8),
+        iterations=1)
 
-(cnts, _) = contours.sort_contours(cnts)
-
-
-rice = int(str(len(cnts))) - 1
-
-print("count:", rice)
-print()
-
-cv2.drawContours(image,cnts,-1,(0,255,0),3)
-cv2.imshow("exp",image)
-cv2.imshow("edge",edged)
-cv2.imshow("gray",gray)
+cv2.imshow("ori",image)
+cv2.imshow("mask",mask)
+cv2.imshow("res",res)
+cv2.imshow("open",open)
 cv2.waitKey(0)
 cv2.destroyAllWindows
